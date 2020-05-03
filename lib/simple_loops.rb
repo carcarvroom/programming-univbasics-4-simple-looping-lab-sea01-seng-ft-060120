e313b60e8f2df17ed621fd36160482579a843360
Carly La <carlylynnla@gmail.com>
2020-05-03 00:44:36 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,3 +1,17 @@
 # Write your methods here
 
-def 
+def loop_message_five_times(message)
+  puts message
+  puts message
+  puts message
+  puts message
+  puts message
+end
+
+def loop_message_n_times(message, n)
+  counter = 0
+  while counter < n
+  puts message
+  counter += 1
+end
+  
